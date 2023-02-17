c70c1115cffb6d90937971f900731bb75c7721d8
CarlosBanda <c.banda07@hotmail.com>
2023-02-17 12:44:47 -0600

    PD POO

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'cband/bienes_raices',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '57ad0ec91a4194aa9f837aa37439337eef263a27',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'cband/bienes_raices' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '57ad0ec91a4194aa9f837aa37439337eef263a27',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
